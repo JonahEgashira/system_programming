14fc05ec4a0f1f98a450c1bfae728f5d2b5894d1
Jonah Egashira <jonahega@gmail.com>
2021-06-12 15:27:31 +0900

    created: multiple pipe shell

--- sh.c
+++ sh.c
@@ -4,19 +4,21 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-char *paths[] = {"/bin/", "/usr/bin/", NULL};
+// char *paths[] = {"/bin/", "/usr/bin/", NULL};
+// char path0[1024];
+// char path1[1024];
 char buf[1024];
 int fds[2] = {};
 char *cmdv[100];
-char *tmp[100];
 char *cmds[100][1024];
-char path0[1024];
 char *cp;
 char *tp;
-int i, j, k, idx;
+int i, j, k;
+int cmd_n;
 int status;
+pid_t ret;
 
-void func(int depth) {
+void parse_input() {
   j = 0;
   k = 0;
   for (i = 0; i < 100; i++) {
@@ -36,7 +38,7 @@ void func(int depth) {
     *tp = '\0';
 
     /* pipe */
-    if (*cp == '>') {
+    if (*cp == '|') {
       j++;
       cmds[j][k] = NULL;
       k = 0;
@@ -50,83 +52,90 @@ void func(int depth) {
     cp = ++tp;
   }
 
-  for (i = 0; i < 5; i++) {
-    for (j = 0; cmds[i][j] != NULL; j++) {
-      printf("cmds%d: %s\n", i, cmds[i][j]);
-    }
+  cmd_n = j + 1;
+
+  // for (i = 0; i < 5; i++) {
+  //   for (j = 0; cmds[i][j] != NULL; j++) {
+  //     printf("cmds%d: %s\n", i, cmds[i][j]);
+  //   }
+  // }
+  // printf("cmd_n: %d\n", cmd_n);
+}
+
+void func(int depth) {
+  /* check all paths */
+  // strcpy(path0, paths[idx0++]);
+  // strcat(path0, cmds[depth][0]);
+  // fprintf(stderr, "Path: %s\n", path0);
+
+  for (i = 0; i < 100; i++) {
+    if (cmds[depth][i] == NULL) continue;
+    // fprintf(stderr, "argv[%d]= %s\n", i, cmds[depth][i]);
   }
 
-  if (!strcmp("exit", cmdv[0])) {
-    fprintf(stderr, "Terminating Shell\n");
-    exit(0);
-  }
-
-  if (fork() == 0) {
-    /* child */
-    if (*cmdv[0] == '/') {
-      /* absolute path */
-      fprintf(stderr, "Path: %s\n", cmdv[0]);
-      for (i = 0; i < 100; i++) {
-        if (cmdv[i] == NULL) continue;
-        fprintf(stderr, "argv[%d]= %s\n", i, cmdv[i]);
-      }
-
-      if (execv(cmdv[0], cmdv) < 0) {
-        /* not successful */
-        fprintf(stderr, "Exec fail: path: %s\n", cmdv[0]);
-        exit(1);
-      }
+  /* exec commands */
+  if (depth == cmd_n - 1) {
+    if (execvp(cmds[0][0], cmds[0]) < 0) {
+      /* error */
+      fprintf(stderr, "Exec fail\n");
     }
+  } else {
+    pipe(fds);
 
-    if (*cmdv[0] == '.') {
-      /* relative path */
-      fprintf(stderr, "Path: %s\n", cmdv[0]);
-      for (i = 0; i < 100; i++) {
-        if (cmdv[i] == NULL) continue;
-        fprintf(stderr, "argv[%d]= %s\n", i, cmdv[i]);
-      }
+    if (fork() == 0) {
+      close(fds[0]);
+      dup2(fds[1], 1);
+      close(fds[1]);
 
-      if (execv(cmdv[0], cmdv) < 0) {
-        fprintf(stderr, "Exec fail: path: %s\n", cmdv[0]);
-        exit(1);
-      }
-    }
+      func(depth + 1);
+    } else {
+      close(fds[1]);
+      dup2(fds[0], 0);
+      close(fds[0]);
 
-    idx = 0;
-
-    do {
-      /* check all paths */
-      strcpy(path0, paths[idx++]);
-      strcat(path0, cmdv[0]);
-      fprintf(stderr, "Path: %s\n", path0);
-      for (i = 0; i < 100; i++) {
-        if (cmdv[i] == NULL) continue;
-        fprintf(stderr, "argv[%d]= %s\n", i, cmdv[i]);
-      }
-
-      /* exec commands */
-
-      if (execv(path0, cmdv) < 0) {
+      if (execvp(cmds[cmd_n - depth - 1][0], cmds[cmd_n - depth - 1]) < 0) {
         /* error */
         fprintf(stderr, "Exec fail\n");
       }
-    } while (paths[idx] != NULL);
-
-  } else {
-    wait(&status);
+    }
   }
 }
 
+void init_shell() {
+  printf("\033[H\033[J");
+  printf("\n\n\n\n**********************************");
+  printf("\n\n\n\t****JONAH SHELL****");
+  printf("\n\n\n\n**********************************");
+  printf("\n\n");
+  printf("Welcome!\n");
+}
+
 int main() {
+  init_shell();
   do {
     /* wait for user input */
     for (i = 0; i < 100; i++) {
       cmdv[i] = NULL;
+      for (j = 0; j < 1024; j++) {
+        cmds[i][j] = NULL;
+      }
     }
-    printf("%% ");
+    printf("> ");
     cp = fgets(buf, sizeof(buf), stdin);
 
-    func(0);
+    parse_input();
+    if (!strcmp("exit", cmds[0][0])) {
+      fprintf(stderr, "Terminating Shell\n");
+      exit(0);
+    }
+
+    ret = fork();
+
+    if (ret == 0) {
+      func(0);
+    } else {
+      wait(&status);
+    }
 
   } while (1);
 }
